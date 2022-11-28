5ad2d874718fdb82422e642b43f5ca2eab89bde1
AlexisCasTawil <alexis_a_k@hotmail.com>
2022-11-27 20:07:03 -0500

    Agrego prueba de tipo de contenido a un elemento

--- src/App.test.js
+++ src/App.test.js
@@ -4,3 +4,8 @@ import App from "./App";
 test("renders the landing page", () => {
   render(<App />);
 });
+
+//Alexis C. Prueba de tipo de contenido a un elemento
+test("typeof of search results counter", () => {
+  expect(typeof ("fs-5")).toBe("string");
+});
