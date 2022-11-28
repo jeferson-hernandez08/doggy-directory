d1873b77ae458aefe6f90159d0ae189fbe862d32
harold <haroldvilla22@gmail.com>
2022-11-27 20:07:42 -0500

    Testeo de verificación de una cadena de caracteres 'Doggy Directory'  (Harold Jaramillo Villa)

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,12 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders the landing page", () => {
+
+// Testeo de verificación de una cadena de caracteres 'Doggy Directory'  (Harold Jaramillo Villa)
+
+test("Debe contener 'Doggy Directory'", () => {
   render(<App />);
+
+  expect(screen.getByRole("heading")).toHaveTextContent(/Doggy Directory/);
 });
+
